221ea69f3eea3039ae1f8ab9cf6f87f2da61c2c1
SaBiT495 <shahedbillah495@gmail.com>
2023-10-07 13:54:15 +0600

    routing and authentication create

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AskQuestion = () => {
+  return (
+    <div className='flex min-h-screen w-full items-center justify-center bg-red-500'>AskQuestion</div>
+  )
+}
+
+export default AskQuestion
